bdfc6830eaa0b540888ff14f44b194120126af05
tigca <84330292+tigca@users.noreply.github.com>
2023-07-11 17:48:48 +0300

    Add files via upload

---
+++ sheep.py
@@ -0,0 +1,26 @@
+#-----Wavatig example python script-----#
+import random
+import time
+
+def remove_sheep():
+    sheep = ["Бэйли", "Ламберт", "Мэри", "Бэзил", "Клотильда"]
+    sheep_str = ", ".join(sheep)
+    print("🧾 Список овец:", sheep_str)
+
+    while sheep:
+        removed_sheep = random.choice(sheep)
+        sheep.remove(removed_sheep)
+        print(f"🤑 Продаем овцу: {removed_sheep}")
+        if len(sheep) == 1:
+            print(f"🧾 Осталась овца: {sheep[0]}")
+        elif sheep:
+            remaining_sheep = ", ".join(sheep)
+            print(f"🧾 Осталось овец: {remaining_sheep}")
+        time.sleep(2)
+        print()
+
+    if not sheep:
+        print("🚫 Ахтунг, овец больше нет!")
+
+if __name__ == "__main__":
+    remove_sheep()
